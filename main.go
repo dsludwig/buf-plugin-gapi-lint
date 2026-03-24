b4df148c2e98d2290745b5b53f16952484e31743
Derek Ludwig <derek.s.ludwig@gmail.com>
2026-03-23 18:51:04 -0700

    feat: add --version flag

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "buf.build/go/bufplugin/check"
+import (
+	"fmt"
+	"os"
+
+	"buf.build/go/bufplugin/check"
+)
+
+// version is set at build time via ldflags.
+var version = "dev"
 
 func main() {
+	for _, arg := range os.Args[1:] {
+		if arg == "--version" || arg == "-v" {
+			fmt.Println(version)
+			os.Exit(0)
+		}
+	}
 	check.Main(buildSpec())
 }
